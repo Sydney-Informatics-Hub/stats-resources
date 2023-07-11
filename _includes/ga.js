6ea77e2651cc31cd66c776bc45ec2f0202eb061c
alexshawsydney <54832440+alexshawsydney@users.noreply.github.com>
2023-07-11 15:44:23 +1000

    Update ga.js

--- _includes/ga.js
+++ _includes/ga.js
@@ -12,7 +12,7 @@ function loadScriptAsync(scriptSrc, callback) {
 
 /* This is the part where you call the above defined function and "calls back" your code which gets executed after the script has loaded */
 
-loadScriptAsync('https://www.googletagmanager.com/gtag/js?id=G-XXXXXXXXXX', function () {
+loadScriptAsync('https://www.googletagmanager.com/gtag/js?id=G-TNWWV3HK57', function () {
     window.dataLayer = window.dataLayer || [];
     function gtag() { dataLayer.push(arguments); }
     gtag('js', new Date());
